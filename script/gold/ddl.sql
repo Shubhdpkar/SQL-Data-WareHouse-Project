a874076fb6f7dc0b67e1f5098b7890cd5426483a
Sourabh Dharmpurikar <114637885+Shubhdpkar@users.noreply.github.com>
2025-08-11 15:23:17 +0530

    Update ddl.sql

--- script/gold/ddl.sql
+++ script/gold/ddl.sql
@@ -1,1 +1,58 @@
+USE DataWarehouse;
+GO
+
+------------------------------------------------------------
+-- View: gold.dim_customers
+-- Purpose:
+--   Creates a customer dimension table combining CRM and ERP data.
+--   Prioritizes CRM as the master source for gender information.
+--   Includes customer personal info, location, and birthdate.
+------------------------------------------------------------
+CREATE OR ALTER VIEW gold.dim_customers AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key, -- Surrogate key
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    la.cntry AS country,
+    ci.cst_material_status AS marital_status,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for Gender info
+        ELSE COALESCE(ca.gen, 'n/a')
+    END AS gender,
+    ca.bdate AS birthdate,
+    ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid;
+GO
+
+
+------------------------------------------------------------
+-- View: gold.dim_products
+-- Purpose:
+--   Creates a product dimension table combining CRM and ERP category data.
+--   Includes only active products (prd_end_dt IS NULL).
+------------------------------------------------------------
+CREATE OR ALTER VIEW gold.dim_products AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate key
+    pn.prd_id AS product_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm AS product_name,
+    pn.cat_id AS category_id,
+    pc.cat AS category,
+    pc.subcat AS subcategory,
+    pc.maintenance,
+    pn.prd_cost AS product_cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS product_start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
+GO
 
